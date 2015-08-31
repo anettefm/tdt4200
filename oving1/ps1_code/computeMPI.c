9c51006d6679c6983a33dcbbd59a36ac4fe5a29c
anettefm <afm_hamster@hotmail.com>
2015-08-31 16:36:48 +0200

    jobbet med computeMPI.c

--- oving1/ps1_code/computeMPI.c
+++ oving1/ps1_code/computeMPI.c
@@ -16,8 +16,11 @@
 */
 
 int main(int argc, char **argv) {
-	int rank, size;
+	int rank, size; /* denne prosessorens rank*/ /* antall prosessorer*/
+    
+    
 
+	
 	if (argc < 3) {
 		printf("This program requires two parameters:\n \
 the start and end specifying a range of positive integers in which \
@@ -29,28 +32,54 @@ start is 2 or greater, and end is greater than start.\n");
 	int stop = atoi(argv[2]);
 
 
+    
 	if(start < 2 || stop <= start){
 		printf("Start must be greater than 2 and the end must be larger than start.\n");
 		exit(1);
 	}
 
 	// TODO: Compute the local range, so that all the elements are accounted for.
+    MPI_Init(NULL, NULL);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    
+    
+    int local_range, rest;
+    local_range=(stop-start)/size;
 
 
 	// Perform the computation
 	double sum = 0.0;
-	for (int i = start; i < stop ; i++) {
+    if(rank==size-1){
+	    rest=mod(stop-start, size);
+    }else{
+        rest=0;
+    }
+    for (int i = start+rank*local_range; i < start+(rank+1)*local_range+rest; i++) {
 		sum += 1.0/log(i);
-	}
+    }
+
 
 	// Debug prints if needed
 
 	// TODO: Get the sum of each node's partial answer
 	// Use MPI_Send and MPI_Recv here
-
+    if(rank != 0){
+        MPI_Send(&sum, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
+    }else{
+        double tot_sum;
+        tot_sum=sum;
+        for(int source=1; source<size; source++){
+            MPI_Recv(&sum, 1, MPI_DOUBLE, source, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            tot_sum+=sum;
+        }
+        
+    }
 	// TODO: Print the global sum once only
-	printf("The sum is: %f\n", sum);
-
+    if(rank = 0){
+	printf("The sum is: %f\n", tot_sum);
+    }
+    MPI_Finalize();
 	return 0;
 }
 
